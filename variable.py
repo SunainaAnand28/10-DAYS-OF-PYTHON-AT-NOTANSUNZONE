42eb6be4553ef480a2caa910c31660a46127757f
Sunaina-Anand <161228634+user895naina@users.noreply.github.com>
2024-05-27 07:50:46 +0530

    DAY 02

---
+++ variable.py
@@ -0,0 +1,9 @@
+#DAY2
+age = int(input("Enter Your age  ")) #variable taking int input from user
+
+print("your age is = ",age)
+
+#DAY 2 of learning is completed.
+
+name = input("Enter your name - ")
+print(name,"your age is ",age)
